bdee09b2c2c40b6113dd9ca949a37586a4243e2e
Simon Clavensjö <142975808+SimonClavensjo@users.noreply.github.com>
2026-04-28 09:08:35 +0200

    add main function

---
+++ services/get-places-data/cmd/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"get-places-data/internal/handlers"
+	"log"
+	"net/http"
+	"os"
+)
+
+
+func main() {    
+    // Basic sanity check, exit immediately if api key is not present 
+    if key := os.Getenv("GOOGLE_API_KEY"); key == "" {
+        panic("Failed to load api key")
+    }
+
+    // Default to 8080
+	port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+
+    // Bind handlers
+    mux := http.NewServeMux()
+    mux.HandleFunc("/barinfo", handlers.GetBarInfoHandler)
+    mux.HandleFunc("/health", handlers.Health)
+
+    srv := &http.Server{
+        Addr:    ":" + port,
+        Handler: mux,
+    }
+
+    log.Println("Starting server on", port)
+    log.Fatal(srv.ListenAndServe())
+}
+
+
