f91461f1b54819f98f8902d0fd0876beda8aa250
Erik Naslund <erik.naslund@posteo.com>
2026-05-28 13:43:59 +0200

    Fix unit creation

--- services/db-client/internal/stores/unit.go
+++ services/db-client/internal/stores/unit.go
@@ -79,48 +79,57 @@ func (s *UnitStore) createBeverage(ctx context.Context, tx pgx.Tx, unit *models.
 	err := tx.QueryRow(ctx, `
         INSERT INTO beverage (name, abv)
         VALUES ($1, $2)
+        ON CONFLICT ON CONSTRAINT beverage_name_abv_unique DO NOTHING
         RETURNING id
-    `,
-		unit.Name,
-		unit.ABV,
-	).Scan(&beverageID)
+    `, unit.Name, unit.ABV).Scan(&beverageID)
 
-	if err != nil {
-		return nil, fmt.Errorf("CreateBeverage: unable to insert new beverage: %w", err)
+	if errors.Is(err, sql.ErrNoRows) {
+		err = tx.QueryRow(ctx, `
+            SELECT id FROM beverage WHERE name = $1 AND abv = $2
+        `, unit.Name, unit.ABV).Scan(&beverageID)
 	}
-
+	if err != nil {
+		return nil, fmt.Errorf("CreateBeverage: %w", err)
+	}
 	return beverageID, nil
 }
 
-func (s *UnitStore) createUnit(ctx context.Context, tx pgx.Tx, unit *models.UnitInput, beverageID *uuid.UUID)	(*uuid.UUID, error) {
-	var UnitID *uuid.UUID
+func (s *UnitStore) createUnit(ctx context.Context, tx pgx.Tx, unit *models.UnitInput, beverageID *uuid.UUID) (*uuid.UUID, error) {
+	var unitID *uuid.UUID
 	err := tx.QueryRow(ctx, `
         INSERT INTO unit (beverage_id, name, volume_ml, size, unit_type)
         VALUES ($1, $2, $3, $4, $5)
+        ON CONFLICT ON CONSTRAINT unit_beverage_volume_type_unique DO NOTHING
         RETURNING id
-    `,
-		beverageID, unit.Name, unit.VolumeML, unit.Size, unit.UnitType,
-	).Scan(&UnitID)
-	if err != nil {
-		return nil, fmt.Errorf("CreateUnit: unable to insert new unit: %w", err)
-	}
+    `, beverageID, unit.Name, unit.VolumeML, unit.Size, unit.UnitType).Scan(&unitID)
 
-	return UnitID, nil
+	if errors.Is(err, sql.ErrNoRows) {
+		err = tx.QueryRow(ctx, `
+            SELECT id FROM unit WHERE beverage_id = $1 AND volume_ml = $2 AND unit_type = $3
+        `, beverageID, unit.VolumeML, unit.UnitType).Scan(&unitID)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("CreateUnit: %w", err)
+	}
+	return unitID, nil
 }
 
 func (s *UnitStore) createVenueUnit(ctx context.Context, tx pgx.Tx, venueID, unitID *uuid.UUID) (*uuid.UUID, error) {
 	var venueUnitID *uuid.UUID
 	err := tx.QueryRow(ctx, `
-        INSERT INTO venue_unit (venue_id, unit_id)
-        VALUES ($1, $2)
-		RETURNING id
-		`,
-		venueID, unitID,
-	).Scan(&venueUnitID)
-	if err != nil {
-		return nil, fmt.Errorf("CreateVenueUnit: unable to insert new venue unit: %w", err)
-	}
+        SELECT id FROM venue_unit WHERE venue_id = $1 AND unit_id = $2
+    `, venueID, unitID).Scan(&venueUnitID)
 
+	if errors.Is(err, sql.ErrNoRows) {
+		err = tx.QueryRow(ctx, `
+            INSERT INTO venue_unit (venue_id, unit_id)
+            VALUES ($1, $2)
+            RETURNING id
+        `, venueID, unitID).Scan(&venueUnitID)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("CreateVenueUnit: %w", err)
+	}
 	return venueUnitID, nil
 }
 
